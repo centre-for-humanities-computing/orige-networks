e6a0a18b388adc4990a8a01e4bb38c9c642a091f
donbjarkone <44224869+donbjarkone@users.noreply.github.com>
2025-09-04 14:02:32 +0200

    Refactored graph data processing: modularized transformations, replaced `main.js` with `Preprocessor` and `GraphModel`, and improved data loading and error handling.

--- src/lib/server/app.js
+++ src/lib/server/app.js
@@ -1,41 +1,80 @@
 import appRootPath from "app-root-path";
 import path from 'node:path';
 import fs from 'node:fs';
-import { loadData, prepareGraphModelForRendering, transformDataToGraph } from "$lib/server/main.js";
+import { Preprocessor } from "$lib/server/Preprocessor.js";
 import * as privateEnvVars from '$env/static/private';
 import Graph from "graphology";
+import { GraphModel } from "$lib/server/GraphModel.js";
 
 const DEVELOPMENT_MODE = privateEnvVars.NODE_ENV === 'development' || privateEnvVars.DEV_MODE === 'true';
+const FORCE_RENDER = true;
 
 const APP_ROOT_PATH = appRootPath.toString();
-
 const APP_DATA_DIR = path.join(APP_ROOT_PATH, 'data');
 const JSON_FILE_PATH = path.join(APP_DATA_DIR, 'references.json');
 
-let entries = loadData(JSON_FILE_PATH);
+// Loads initial JSON data
+let data = loadJSONData(JSON_FILE_PATH);
 
-let { nodes, edges, facets } = transformDataToGraph(entries);
+let preprocessor = new Preprocessor();
+let results = preprocessor.preprocess(data.entries);
 
-let graph;
+const nodes = results.nodes;
+const edges = results.edges;
+const facets = results.facets;
+const errors = results.errors;
 
-const GRAPH_FILE_PATH = "/Users/au335497/Temp/orige-net/graph.json";
+if (errors.length > 0) {
+    const ERROR_FILE_PATH = "/Users/au335497/Temp/orige-net/errors.json";
+    saveJSONData(ERROR_FILE_PATH, errors);
+}
 
-if (DEVELOPMENT_MODE) {
+let graphModel;
+
+// DEV MODE
+if (DEVELOPMENT_MODE && !FORCE_RENDER) {
+    const GRAPH_FILE_PATH = "/Users/au335497/Temp/orige-net/graph.json"; // Change in prod.
+
     try {
         if (fs.existsSync(GRAPH_FILE_PATH)) {
-            let fileContent = fs.readFileSync(GRAPH_FILE_PATH, 'utf-8');
-            graph = Graph.from(JSON.parse(fileContent));
+            graphModel = Graph.from(loadJSONData(GRAPH_FILE_PATH));
         } else {
-            graph = prepareGraphModelForRendering(nodes, edges);
-            fs.writeFileSync(GRAPH_FILE_PATH, JSON.stringify(graph.export()))
+            graphModel = new GraphModel(nodes, edges, facets);
+            graphModel.init();
+
+            saveJSONData(GRAPH_FILE_PATH, graphModel.export())
         }
     } catch (err) {
         console.error('Something went wrong. Creating new graph.')
     }
 } else {
-    graph = prepareGraphModelForRendering(nodes, edges);
+    // PRODUCTION
+    graphModel = new GraphModel(nodes, edges, facets);
+    graphModel.init();
 }
 
-let nodeIDs = new Set(graph.nodes())
+export { graphModel }
 
-export { graph, facets, nodeIDs }
+/**
+ * Main data loader function
+ * @param {string} filePath
+ * @return {*}
+ */
+function loadJSONData(filePath) {
+    try {
+        let content = fs.readFileSync(filePath, 'utf-8');
+        return JSON.parse(content);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+function saveJSONData(filePath, data) {
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data), 'utf-8');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
