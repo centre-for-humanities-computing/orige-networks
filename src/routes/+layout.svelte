eda6e906cd4dd8ec78d2c325c6732a6df9d6e202
Bjarke Vognstrup Fog <bfog@cc.au.dk>
2025-10-29 11:47:11 +0100

    Remove redundant `resolveURL` function and update links to use `resolve` directly
    - Simplify path resolution logic in `+layout.svelte` by replacing `resolveURL` usage with `resolve`.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,22 @@
 <script>
  import "$lib/client/assets/styles.css"
- import { base } from '$app/paths';
+ import { resolve } from '$app/paths';
  import { setContext } from "svelte";
  import { CONTEXT } from "$lib/utils/constants.js";
 
  let { children } = $props();
 
- /**
-  * Resolves a path
-  * @param {string} path
-  */
- function resolveURL(path) {
-  return `${base}${path}`;
- }
-
- setContext(CONTEXT.RESOLVE_URL, resolveURL);
-
 </script>
 
 <nav>
  <div class="nav-item">
-  <a href={resolveURL("/")}>Orige.net</a>
+  <a href={resolve("/")}>Orige.net</a>
  </div>
     <div class="nav-item">
-        <a href={resolveURL("/glossaries")}>Glossaries</a>
+        <a href={resolve("/glossaries")}>Glossaries</a>
     </div>
  <div class="nav-item">
-  <a href={resolveURL("/about")}>About</a>
+  <a href={resolve("/about")}>About</a>
  </div>
 </nav>
   {@render children()}
