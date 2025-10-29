2add5af58f2e9e9878fe46f0e369b5838628abdc
Bjarke Vognstrup Fog <bfog@cc.au.dk>
2025-10-29 11:46:16 +0100

    Add JSDoc annotations for `isNotNullNotEmptyString` and `caseInsensitiveStringSearch` methods
    - Improve type safety and code documentation with detailed JSDoc for utility functions.

--- src/lib/utils/helpers.js
+++ src/lib/utils/helpers.js
@@ -1,11 +1,14 @@
-
-
+/**
+ * Whether a string is not null and not empty, i.e., has a length > 0
+ * @param {string} str
+ * @return {boolean}
+ */
 export function isNotNullNotEmptyString(str) {
     return str && typeof str === "string" && str.length > 0;
 }
 
 /**
- *
+ * Case-insensitive string search method
  * @param {string} str
  * @param {string} searchStr
  * @return {boolean}
